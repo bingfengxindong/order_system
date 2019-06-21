266473a1040657adf4461345f048fe478a06b6b0
zhangpf <15736950639@163.com>
2019-06-21 09:13:41 +0800

    add accounting_documents js verify and data calculate

--- static/js/orderadd.js
+++ static/js/orderadd.js
@@ -1,4 +1,9 @@
 $(function () {
+    switch_navigation_main();
+    no_negative_main();
+    total_price_main();
+});
+function switch_navigation_main() {
     var proofing_progress_click = $("#proofing-progress-click");
     var proofing_progress = $("#proofing-progress");
     var production_schedule_click = $("#production-schedule-click");
@@ -21,11 +26,41 @@ $(function () {
     accounting_documents_click.click(function () {
         switch_navigation(accounting_documents_click,accounting_documents,quotation_click,quotation,production_schedule_click,production_schedule,proofing_progress_click,proofing_progress);
     });
+}
 
+function switch_navigation(sc,s,hc1,h1,hc2,h2,hc3,h3){
+    sc.attr("class","center-title-in-now");
+    hc1.attr("class","center-title-in");
+    hc2.attr("class","center-title-in");
+    hc3.attr("class","center-title-in");
+    s.show();
+    h1.hide();
+    h2.hide();
+    h3.hide();
+}
+
+function no_negative_main() {
     var order_number = $(".order-number");
     // 数量不能为负数
     no_negative(order_number);
+}
 
+function no_negative(a) {
+    a.change(function () {
+        var num_val = $(this).val();
+        if(num_val < 0){
+            $(this).val(0);
+        }
+    });
+    a.keyup(function () {
+        var num_val = $(this).val();
+        if(num_val < 0){
+            $(this).val(0);
+        }
+    });
+}
+
+function total_price_main() {
     // 数量单价覆盖全表 总价计算
     var pi_amount = $("#pi_amount");
     var pi_unit_price = $("#pi_unit_price");
@@ -45,107 +80,8 @@ $(function () {
     pi_unit_price.keyup(function () {
         change_unit_price(pi_unit_price,pi_amount,pi_total_price)
     });
-
-    // 大货报价计算
-    var pi_offer = $("#pi_offer");
-    var pi_fabric_quotation = $("#pi_fabric_quotation");
-    var pi_ingredients_quotation = $("#pi_ingredients_quotation");
-    var pi_labor_payment_quotation = $("#pi_labor_payment_quotation");
-    var pi_water_washing_quotation = $("#pi_water_washing_quotation");
-    var pi_print_quotation = $("#pi_print_quotation");
-    var pi_embroider_quotation = $("#pi_embroider_quotation");
-    var pi_packaging_quotation = $("#pi_packaging_quotation");
-    var pi_other_quotation = $("#pi_other_quotation");
-    var pi_reserved_profits = $("#pi_reserved_profits");
-
-    pi_fabric_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_fabric_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_ingredients_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_ingredients_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_labor_payment_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_labor_payment_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_water_washing_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_water_washing_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_print_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_print_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_embroider_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_embroider_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_packaging_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_packaging_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_other_quotation.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_other_quotation.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-
-    pi_reserved_profits.change(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-    pi_reserved_profits.keyup(function () {
-        calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits)
-    });
-});
-
-function switch_navigation(sc,s,hc1,h1,hc2,h2,hc3,h3){
-    sc.attr("class","center-title-in-now");
-    hc1.attr("class","center-title-in");
-    hc2.attr("class","center-title-in");
-    hc3.attr("class","center-title-in");
-    s.show();
-    h1.hide();
-    h2.hide();
-    h3.hide();
 }
-function no_negative(a) {
-    a.change(function () {
-        var num_val = $(this).val();
-        if(num_val < 0){
-            $(this).val(0);
-        }
-    });
-    a.keyup(function () {
-        var num_val = $(this).val();
-        if(num_val < 0){
-            $(this).val(0);
-        }
-    });
-}
+
 function change_amount(pi_amount,pi_unit_price,pi_total_price) {
     var pi_amount_val = pi_amount.val();
 
@@ -171,22 +107,5 @@ function change_unit_price(pi_unit_price,pi_amount,pi_total_price) {
     }else{
         unit_price = parseFloat(pi_unit_price_val);
         pi_total_price.val((pi_amount_val * unit_price).toFixed(2));
-    }
-}
-function calculate_offer(pi_offer,pi_fabric_quotation,pi_ingredients_quotation,pi_labor_payment_quotation,pi_water_washing_quotation,pi_print_quotation,pi_embroider_quotation,pi_packaging_quotation,pi_other_quotation,pi_reserved_profits) {
-    var pi_offer_val = pi_offer.val();
-    var pi_fabric_quotation_val = pi_fabric_quotation.val();
-    var pi_ingredients_quotation_val = pi_ingredients_quotation.val();
-    var pi_labor_payment_quotation_val = pi_labor_payment_quotation.val();
-    var pi_water_washing_quotation_val = pi_water_washing_quotation.val();
-    var pi_print_quotation_val = pi_print_quotation.val();
-    var pi_embroider_quotation_val = pi_embroider_quotation.val();
-    var pi_packaging_quotation_val = pi_packaging_quotation.val();
-    var pi_other_quotation_val = pi_other_quotation.val();
-    var pi_reserved_profits_val = pi_reserved_profits.val();
-
-    if(pi_fabric_quotation_val && pi_ingredients_quotation_val && pi_labor_payment_quotation_val && pi_water_washing_quotation_val && pi_print_quotation_val && pi_embroider_quotation_val && pi_packaging_quotation_val && pi_other_quotation_val && pi_reserved_profits_val){
-        pi_offer_val = parseFloat(pi_fabric_quotation_val) + parseFloat(pi_ingredients_quotation_val) + parseFloat(pi_labor_payment_quotation_val) + parseFloat(pi_water_washing_quotation_val) + parseFloat(pi_print_quotation_val) + parseFloat(pi_embroider_quotation_val) + parseFloat(pi_packaging_quotation_val) + parseFloat(pi_other_quotation_val) + parseFloat(pi_reserved_profits_val);
-        pi_offer.val(parseFloat(pi_offer_val).toFixed(2));
     }
 }
