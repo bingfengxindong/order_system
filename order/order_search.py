5397a3eefccace9a566e42aa49b9280fb5ca6582
zhangpf <15736950639@163.com>
2019-07-10 14:23:28 +0800

    edit orderlist.html image

--- order/order_search.py
+++ order/order_search.py
@@ -7,7 +7,7 @@ class OrderPP(View):
         orders = Order.objects.filter(o_pp_all_end=False).order_by("-pk")
         orders_len = len(orders)
         context = {
-            "title": "订单列表",
+            "title": "订单打样中",
             "orders": orders,
             "orders_len": orders_len,
         }
@@ -18,7 +18,7 @@ class OrderPS(View):
         orders = Order.objects.filter(o_pp_all_end=True,o_productionschedule__ps_end=False).order_by("-pk")
         orders_len = len(orders)
         context = {
-            "title": "订单列表",
+            "title": "订单大货中",
             "orders": orders,
             "orders_len": orders_len,
         }
@@ -29,7 +29,7 @@ class OrderO(View):
         orders = Order.objects.filter(o_productionschedule__ps_end=True,o_end=False).order_by("-pk")
         orders_len = len(orders)
         context = {
-            "title": "订单列表",
+            "title": "订单未完成",
             "orders": orders,
             "orders_len": orders_len,
         }
@@ -40,7 +40,7 @@ class OrderEnd(View):
         orders = Order.objects.filter(o_end=True).order_by("-pk")
         orders_len = len(orders)
         context = {
-            "title": "订单列表",
+            "title": "订单已完成",
             "orders": orders,
             "orders_len": orders_len,
         }
